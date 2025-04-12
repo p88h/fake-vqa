088824560e5b41df0e4b7d2327708770fe8b76c0
Staszek Pasko <staszek@gmail.com>
2025-04-12 15:26:03 +0200

    Add OCR task. Add parameters for page control.

--- fakevqa_benchmark.py
+++ fakevqa_benchmark.py
@@ -9,15 +9,17 @@ from dataclasses import asdict
 from collections import defaultdict
 from generate_benchmark import generate_benchmark_dataset
 
+# tag -> (question, max_tokens)
 question_bank = {
-    "simple": "What is the value of the keyword?",
-    "qa": """Answer following questions based on the document:
+    "simple": ( "What is the value of the keyword?", 128 ),
+    "qa": ( """Answer following questions based on the document:
     Answer one line per question, just the answer, no other text.
     - docid: document ID
     - title: title of the document
     - keyword: value of the keyword
-    - param_b: the value of the parameter B in the table""",
-    "summarize": "Summarize the document in 500 words"
+    - param_b: the value of the parameter B in the table""", 1024 ),
+    "summarize": ( "Summarize the document in 500 words", 2048 ),
+    "ocr": ( "Extract text from the document", 4096 )
 }
 
 def update_prompt(prompt, question):
@@ -68,14 +70,16 @@ def grouped_benchmark(llm, sampling_params, lora_requests, prompts, tag):
     # Group prompts by number of pages (length of image data)
     grouped_prompts = defaultdict(list)
     for p in prompts:
-        q = update_prompt(p, question_bank[tag])
+        q = update_prompt(p, question_bank[tag][0])
         grouped_prompts[len(p["multi_modal_data"]["image"])].append(q)
 
     start_time = time.time()
     metrics = []
     outputs = []
+    sampling_params.max_tokens = question_bank[tag][1]
     for page_count, group in grouped_prompts.items():
-        print(f"Running group with {page_count} pages for {tag} question")
+        print(f"Running group with {page_count} pages for {tag} question (max output: "
+              f"{question_bank[tag][1]} tokens) ...")
         outputs.extend(llm.generate(
             group,
             sampling_params=sampling_params,
@@ -92,10 +96,12 @@ def grouped_benchmark(llm, sampling_params, lora_requests, prompts, tag):
 def mixed_benchmark(llm, sampling_params, lora_requests, prompts, tag):
     # Second time - run a quality check on all the documents with the real question
     # This can perhaps also measure cached performance -- all images are the same
-    print(f"Running a mixed benchmark for {tag} question ...")
+    print(f"Running a mixed benchmark for {tag} question (max output: "
+          f"{question_bank[tag][1]} tokens) ...")
     start_time = time.time()
+    sampling_params.max_tokens = question_bank[tag][1]
     outputs = llm.generate(
-        [update_prompt(p, question_bank[tag]) for p in prompts],
+        [update_prompt(p, question_bank[tag][0]) for p in prompts],
         sampling_params=sampling_params,
         lora_request=lora_requests,
     )
@@ -133,18 +139,32 @@ def composite_scenario(entries):
     Run a composite scenario with throughput/simple, mixed/qa, and mixed/summarize benchmarks.
     """
     llm, sampling_params, lora_requests, prompts = prepare_prompts(entries)
+
+    # Single question, grouped by size, used to measure throughput / memory usage
     _, metrics = grouped_benchmark(llm, sampling_params, lora_requests, prompts, "simple")
+
+    # QA task
     outputs, metrics1 = mixed_benchmark(llm, sampling_params, lora_requests, prompts, "qa")
+    metrics.extend(metrics1)
     answers = evaluate_answers(outputs, entries)
     # save answers to a file for review
     with open("answers.json", "w") as f:
         json.dump(answers, f, indent=2)
+
+    # Summarize task
     outputs, metrics2 = mixed_benchmark(llm, sampling_params, lora_requests, prompts, "summarize")
+    metrics.extend(metrics2)
     # save summaries to a file for review
     with open("summaries.json", "w") as f:
         json.dump({entries[i]["title"]: o.outputs[0].text for i, o in enumerate(outputs)}, f, indent=2)
-    metrics.extend(metrics1)
-    metrics.extend(metrics2)
+
+    # OCR task
+    outputs, metrics3 = mixed_benchmark(llm, sampling_params, lora_requests, prompts, "ocr")
+    metrics.extend(metrics3)
+    # save ocr to a file for review
+    with open("ocr.json", "w") as f:
+        json.dump({entries[i]["title"]: o.outputs[0].text for i, o in enumerate(outputs)}, f, indent=2)
+
     save_metrics(metrics, "composite", "all")
 
 def grouped_scenario(entries, tag):
@@ -186,7 +206,9 @@ def throughput_scenario(entries, tag):
     save_metrics(all_metrics, "throughput", tag)
 
 def main(args):
-    entries = generate_benchmark_dataset(max_pages=args.num_pages)
+    entries = generate_benchmark_dataset(max_pages=args.max_pages, 
+                                         min_pages=args.min_pages, 
+                                         num_pages=args.total_pages)
     if args.scenario == "composite":
         composite_scenario(entries)
     elif args.scenario == "grouped":
@@ -209,11 +231,27 @@ if __name__ == "__main__":
         help="Set the seed when initializing `vllm.LLM`.",
     )
     parser.add_argument(
-        "--num-pages",
-        "-n",
-        choices=[4, 8, 16, 32, 64, 128, 256],
+        "--max-pages",
+        "-m",
+        type=int,
+        choices=[4, 8, 16, 32, 64, 128],
         default=32,
         help="Maximum number of page images to use for the benchmark.",
+    )
+    parser.add_argument(
+        "--min-pages",
+        "-n",
+        type=int,
+        choices=[1, 2, 4, 8, 16, 32],
+        default=4,
+        help="Minimum number of page images to use for the benchmark.",
+    )
+    parser.add_argument(
+        "--total-pages",
+        "-p",
+        type=int,
+        default=None,
+        help="Total number of page images to use for the benchmark.",
     )
     parser.add_argument(
         "--tag",
